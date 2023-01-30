44675cf1203432bb2415c810ba65a2e13f218ed6
NomadiCxac <christianxavier.cordero@gmail.com>
2023-01-30 12:36:52 -0500

    added a js file to attempt creating a modal for item cards. Currently non-operational

---
+++ index.js
@@ -0,0 +1,19 @@
+itemPopUp.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+var modal = document.getElementById("itemModal");
+var itemPopUp = document.getElementById("itemCard");
+var span = document.getElementById("close")[0];
